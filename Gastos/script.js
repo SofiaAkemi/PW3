cbd16bd4a69dc7b07c86d473921f856ff0203236
SofiaAkemi <sofia.kucinskis@gmail.com>
2026-04-21 23:45:52 -0300

    HTML_JS_Funcionais_23h45_21/04

--- Gastos/script.js
+++ Gastos/script.js
@@ -20,27 +20,6 @@ function salvarGastos(){
     //Código para converter o array em texto ("stringify")
 }
 
-//Adicionando um gasto
-function adicionarGasto(descricao, valor, categoria){
-    const gasto = {descricao: descricao, valor: valor, categoria: categoria}; //Criamos um objeto para um gasto
-
-    gastos.push(gasto); //Adiciona o gasto no array "gastos"
-    salvarGastos(); //Puxa a função
-    renderizarTela(); //Puxa a função
-}
-
-//Calculando o total dos gastos
-function calcularTotalGasto(){
-    let total = 0; //Cria variável para o total
-
-    gastos.forEach(g => {
-        total += g.valor;
-    });
-    //Verifica o valor de cada gasto para somar um a um
-
-    document.getElementById("ttl").innerHTML = "O Valor Total Gasto é: R$" + total.toFixed(2); //Joga isso na tela
-}
-
 //Puxando do Local Storage
 function carregarDados(){
     const dados = localStorage.getItem("gastos");
@@ -52,29 +31,54 @@ function carregarDados(){
     //Converte tudo de texto para array caso hajam dados
 }
 
+//Adicionando um gasto
+function adicionarGasto(descricao, valor, categoria){
+    const gasto = {descricao, valor, categoria}; //Criamos um objeto para um gasto
+
+    gastos.push(gasto); //Adiciona o gasto no array "gastos"
+    salvarGastos(); //Puxa a função
+    renderizarTela(); //Puxa a função
+}
+
+//Calculando o total dos gastos
+function calcularTotalGasto(){
+    let total = 0; //Cria variável para o total
+
+    gastos.forEach(g => {
+        total += Number(g.valor);
+    });
+    //Verifica o valor de cada gasto para somar um a um
+
+    document.getElementById("ttl").innerHTML = "O Valor Total Gasto é: R$" + total.toFixed(2); //Joga isso na tela
+}
+
 //Atualizando a tela
 function renderizarTela(lista = gastos){
     const ptListaGastos = document.getElementById("gastosLayout");
     ptListaGastos.innerHTML = "";
 
-    lista.forEach((gasto, index) => {
+    lista.forEach((gasto) => {
         const gastoNaLista = document.createElement("div"); //Cria um div para cada gasto
-        gastoNaLista.innerHTML =
-        `<p>${gasto.descricao}<br>
-         R$${gasto.valor.toFixed(2)}<br>
-         ${categoriasNome[gasto.categoria]}<br>
-         <button onClick="removerGasto(${index})">Remover Gasto</button>
+        gastoNaLista.innerHTML = `
+        <p>
+            ${gasto.descricao}<br>
+            R$${gasto.valor.toFixed(2)}<br>
+            ${categoriasNome[gasto.categoria]}<br><br>
+            <button type="button" onClick="removerGasto('${gasto.descricao}', ${gasto.valor}, '${gasto.categoria}')">
+                Remover Gasto
+            </button>
         </p>`; //Conteúdo
+
         ptListaGastos.appendChild(gastoNaLista); //Põe o gasto na tela
     });
-    //O forEach pega: (gasto(ou seja, o elemento), index(posição no array))
+    //O forEach pega o elemento e sua posição no array
 
     calcularTotalGasto(); //Puxa a função
 }
 
 //Removendo um gasto do Local Storage
-function removerGasto(index){
-    gastos.splice(index, 1); //O splice remove 1 item, começando no index
+function removerGasto(descricao, valor, categoria){
+    gastos = gastos.filter(g => !(g.descricao === descricao && g.valor === valor && g.categoria === categoria)); //Filtra os gastos, selecionando o que NÃO for igual ao selecionado
     salvarGastos(); //Puxa a função
     renderizarTela(); //Puxa a função
 }
@@ -83,32 +87,52 @@ function removerGasto(index){
 function filtrarGastos(categoria){
     let gastosFiltrados = []; //Array para os gastos filtrados
 
-    //A estrutura switch
     switch(categoria){
-        //Caso seja escolhido "Todos"
         case "todos":
             gastosFiltrados = gastos;
             break;
 
-        //Caso seja escolhido algum dos outros
         case "alimentacao":
-        case "lazer":
-        case "transporte":
-        case "saude":
-        case "trabalho":
-        case "escola":
-        case "assinatura":
-        case "conta":
-        case "outro":
-            gastosFiltrados = gastos.filter(g => g.categoria === categoria); //=== verifica se é o mesmo valor e tipo
+            gastosFiltrados = gastos.filter(g => g.categoria === "alimentacao"); //Filtra com a categoria do g, para tudo que for igual a Alimentação
             break;
 
-        //Por precaução, no caso de algum problema
+        case "lazer":
+            gastosFiltrados = gastos.filter(g => g.categoria === "lazer"); //Filtra com a categoria do g, para tudo que for igual a Lazer
+            break;
+
+        case "transporte":
+            gastosFiltrados = gastos.filter(g => g.categoria === "transporte"); //Filtra com a categoria do g, para tudo que for igual a Transporte
+            break;
+
+        case "saude":
+            gastosFiltrados = gastos.filter(g => g.categoria === "saude"); //Filtra com a categoria do g, para tudo que for igual a Saúde
+            break;
+
+        case "trabalho":
+            gastosFiltrados = gastos.filter(g => g.categoria === "trabalho"); //Filtra com a categoria do g, para tudo que for igual a Trabalho
+            break;
+
+        case "escola":
+            gastosFiltrados = gastos.filter(g => g.categoria === "escola"); //Filtra com a categoria do g, para tudo que for igual a Escola
+            break;
+
+        case "assinatura":
+            gastosFiltrados = gastos.filter(g => g.categoria === "assinatura"); //Filtra com a categoria do g, para tudo que for igual a Assinatura
+            break;
+
+        case "conta":
+            gastosFiltrados = gastos.filter(g => g.categoria === "conta"); //Filtra com a categoria do g, para tudo que for igual a Conta
+            break;
+
+        case "outro":
+            gastosFiltrados = gastos.filter(g => g.categoria === "outro"); //Filtra com a categoria do g, para tudo que for igual a Outro
+            break;
+
         default:
-            gastosFiltrados = gastos;
+            gastosFiltrados = gastos; //O pré definido, por precaução, são todos
     }
 
-    renderizarTela(gastosFiltrados); //Carrega somente os gastos desejados
+    renderizarTela(gastosFiltrados);
 }
 
 //Evento que ocorre ao enviar o formulário
@@ -117,11 +141,12 @@ document.getElementById("formGasto").addEventListener("submit", function(event) 
     event.preventDefault(); //Evita que a página recaregue
 
     //Pegando dados do formuário
-    const desc = document.getElementById("desc").value;
-    let val = document.getElementById("val").value;
+    const desc = document.getElementById("desc").value.trim();
+    let val = document.getElementById("val").value.trim();
     const categoria = document.getElementById("categoria").value;
 
-    val = Number(val.replace(",", ".")); //Troca , por . e define a variável como número
+    val = val.replace(",", "."); //Troca , por .
+    val = parseFloat(val); //Define a variável como float
 
     //Verificando se os valores estão corretos (que os campos estão preenchidos com os dados certos)
     if (desc === "" || isNaN(val) || val <= 0){  //NaN -> Not a Number, || -> OR
